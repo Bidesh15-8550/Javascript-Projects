321499918535c0397c954608a6ebddbb7f4b6bf5
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-06 03:56:47 +0600

    using github api

---
+++ Github Profiles/main.js
@@ -0,0 +1,23 @@
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getUser("florinpop17");
+
+async function getUser(username) {
+    const resp = await fetch(APIURL + username);
+    const respData = await resp.json();
+
+    createUserCard(respData);
+
+    getRepos(username);
+}
+
+async function getRepos(username) {
+    const resp = await fetch(APIURL + username + "/repos");
+    const respData = await resp.json();
+
+    addReposToCard(respData);
+}
