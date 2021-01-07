a306f2f6e0c1b9db8631ce64bbfa66b981ef4ab3
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-08 00:01:43 +0600

    js

--- Drawing App/main.js
+++ Drawing App/main.js
@@ -5,3 +5,17 @@ const sizeEl = document.getElementById("size");
 const colorEl = document.getElementById("color");
 const clearEl = document.getElementById("clear");
 const ctx = canvas.getContext("2d");
+
+
+let size = 30;
+let isPressed = false;
+let color = "black";
+let x = undefined;
+let y = undefined;
+
+canvas.addEventListener("mousedown", (e) => {
+    isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
+});
