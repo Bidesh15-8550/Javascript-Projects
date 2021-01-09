920d77f38ffcf29bf103b24641a611bd7598195c
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-10 00:43:53 +0600

    Done!

--- Movie App/main.js
+++ Movie App/main.js
@@ -50,3 +50,25 @@ function showMovies(movies) {
         main.appendChild(movieEl);
     });
 }
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
