674a7df0d4495371c0675bce8d75538b5e95b74c
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-12 00:16:19 +0600

    js

--- Password generator/main.js
+++ Password generator/main.js
@@ -7,3 +7,55 @@ const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
 const generateEl = document.getElementById("generate");
 
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
+
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword() {
+    const len = lenEl.value;
+
+    let password = "";
+
+    if (upperEl.checked) {
+        password += getUppercase();
+    }
+
+    if (lowerEl.checked) {
+        password += getLowercase();
+    }
+
+    if (numberEl.checked) {
+        password += getNumber();
+    }
+
+    if (symbolEl.checked) {
+        password += getSymbol();
+    }
+
+    for (let i = password.length; i < len; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
+}
+
+
