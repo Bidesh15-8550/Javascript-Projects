0bf3a788e4bf537e98218b7d229013f80c5ba645
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-12 00:15:54 +0600

    js

---
+++ Password generator/main.js
@@ -0,0 +1,9 @@
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
