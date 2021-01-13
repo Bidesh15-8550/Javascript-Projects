1be3d33ad94ba89b1272881b563f2628855a91c6
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-14 03:14:31 +0600

    js

--- Recipe app with API/main.js
+++ Recipe app with API/main.js
@@ -9,3 +9,25 @@ const searchBtn = document.getElementById("search");
 
 getRandomMeal();
 fetchFavMeals();
+
+
+async function getRandomMeal() {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+
+    addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+    );
+
+    const respData = await resp.json();
+    const meal = respData.meals[0];
+
+    return meal;
+}
