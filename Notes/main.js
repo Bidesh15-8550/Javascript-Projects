4d2101aa95e97f258a19c528ee2a2c50694afd5a
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-16 01:16:01 +0600

    js

--- Notes/main.js
+++ Notes/main.js
@@ -26,3 +26,23 @@ function addNewNote(text = "") {
             <textarea class="${text ? "hidden" : ""}"></textarea>
         </div>
     `;
+
+    const editBtn = note.querySelector(".edit");
+    const deleteBtn = note.querySelector(".delete");
+
+    const main = note.querySelector(".main");
+    const textArea = note.querySelector("textarea");
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
+    editBtn.addEventListener("click", () => {
+        main.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+    });
+
+    deleteBtn.addEventListener("click", () => {
+        note.remove();
+
+        updateLS();
+    });
