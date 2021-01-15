69e653e7e78bab5dcf4be59e88d2e392765bc57a
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-01-16 01:15:41 +0600

    js

--- Notes/main.js
+++ Notes/main.js
@@ -11,3 +11,18 @@ if (notes) {
 addBtn.addEventListener("click", () => {
     addNewNote();
 });
+
+function addNewNote(text = "") {
+    const note = document.createElement("div");
+    note.classList.add("note");
+
+    note.innerHTML = `
+        <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>
+        </div>
+    `;
